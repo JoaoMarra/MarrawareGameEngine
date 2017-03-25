531a2f503579ec7477b759fd2e503fb9bda0e106
João Gabriel <silvamarraster@gmail.com>
2017-03-25 15:48:41 -0300

    first commit

---
+++ android/src/com/marraware/gameengine/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.marraware.gameengine;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.marraware.gameengine.MarrawareGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MarrawareGame(), config);
+	}
+}
---
+++ desktop/src/com/marraware/gameengine/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.marraware.gameengine.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.marraware.gameengine.MarrawareGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MarrawareGame(), config);
+	}
+}
